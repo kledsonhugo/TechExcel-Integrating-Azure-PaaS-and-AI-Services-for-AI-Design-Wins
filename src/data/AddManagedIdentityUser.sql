a39e0755652221e5f86568d9a3e9be8c70dce959
Kledson Basso <kledsonhugo@gmail.com>
2025-04-08 16:30:26 -0300

    Exercise 02, Step 01, Parte 07, Deploy Web API service

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER "id-dhhd6z3f6g2xu" FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER "id-dhhd6z3f6g2xu";
+ALTER ROLE db_datawriter ADD MEMBER "id-dhhd6z3f6g2xu";
 
